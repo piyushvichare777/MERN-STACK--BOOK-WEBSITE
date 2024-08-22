4dabb0382b01364694c0bc14665557aac829dc56
Piyush Vichare <125631663+piyushvichare777@users.noreply.github.com>
2024-08-22 19:31:15 +0530

    Updates index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,42 +3,30 @@ import dotenv from "dotenv";
 import connectDB from "./DB/const.js";
 import bookrouter from "./routes/books.routes.js";
 import userrouter from "./routes/user.route.js";
-import cors from "cors"
+import cors from "cors";
+
 dotenv.config();
-
-
 
 const app = express();
 const port = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(express.json());
-
 app.use(cors({
-  origin: 'https://silver-sunflower-d0a53a.netlify.app/', 
+  origin: 'https://silver-sunflower-d0a53a.netlify.app', 
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
 
-
+app.use(express.json());
 connectDB(MONGODB_URI);
 
 app.get("/", (req, res) => {
   res.send("running successfully");
 });
 
-// Defining routes
 app.use("/book", bookrouter);
-app.use("/user", userrouter)
-
-// --------------------------deployment------------------------------
-
-
-// --------------------------deployment------------------------------
-
-
-
+app.use("/user", userrouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
